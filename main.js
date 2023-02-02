21d99ac0e7376d1abd701ea5fa962175a65704b2
OilyPotato819 <o.schneider@share.epsb.ca>
2023-02-02 14:48:22 -0700

    done

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 // Functionize Minecraft Fishing Start Code
 
 // Variables to store HTML Elements
-let fishBtn = document.getElementById('fish-btn');
-let charSelect = document.getElementById('character-select');
-let resultImg = document.getElementById('result-img');
-let numCodSpan = document.getElementById('num-cod');
-let numSalmonSpan = document.getElementById('num-salmon');
-let numTropicalSpan = document.getElementById('num-tropical');
-let numPufferSpan = document.getElementById('num-puffer');
+let fishBtn = document.getElementById("fish-btn");
+let charSelect = document.getElementById("character-select");
+let resultImg = document.getElementById("result-img");
+let spans = {
+  cod: document.getElementById("num-cod"),
+  salmon: document.getElementById("num-salmon"),
+  tropical: document.getElementById("num-tropical"),
+  puffer: document.getElementById("num-puffer"),
+};
 
 // Global Variables
-let numCod = 0;
-let numSalmon = 0;
-let numTropical = 0;
-let numPuffer = 0;
+let numFish = {
+  cod: 0,
+  salmon: 0,
+  tropical: 0,
+  puffer: 0,
+};
 
 // Add Event Listener to Calculate Button
-fishBtn.addEventListener('click', fishBtnClicked);
+fishBtn.addEventListener("click", fishBtnClicked);
 
 // Event handler for fishBtn
 function fishBtnClicked() {
@@ -24,45 +28,35 @@ function fishBtnClicked() {
   let character = charSelect.value;
 
   // Catch fish based on character
-  if (character === 'steve') {
+  if (character === "steve") {
     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-    let randNum = Math.random();
-    if (randNum < 0.7) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.9) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.95) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
-  } else if (character === 'alex') {
+    catchFish(0.7, 0.9, 0.95);
+  } else if (character === "alex") {
     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-    let randNum = Math.random();
-    if (randNum < 0.1) {
-      numCod++;
-      numCodSpan.innerHTML = numCod;
-      resultImg.src = 'img/Raw-Cod.png';
-    } else if (randNum < 0.2) {
-      numSalmon++;
-      numSalmonSpan.innerHTML = numSalmon;
-      resultImg.src = 'img/Raw-Salmon.png';
-    } else if (randNum < 0.5) {
-      numTropical++;
-      numTropicalSpan.innerHTML = numTropical;
-      resultImg.src = 'img/Tropical-Fish.png';
-    } else {
-      numPuffer++;
-      numPufferSpan.innerHTML = numPuffer;
-      resultImg.src = 'img/Pufferfish.png';
-    }
+    catchFish(0.1, 0.2, 0.5);
+  } else {
+    // Villager: Cod 25%, Salmon 25%, Tropical 25%, Puffer 25%
+    catchFish(0.25, 0.5, 0.75);
   }
+}
+
+function catchFish(codProb, salmonProb, tropicalProb) {
+  let randNum = Math.random();
+
+  if (randNum < codProb) {
+    caughtFish("cod", "Raw-Cod");
+  } else if (randNum < salmonProb) {
+    caughtFish("salmon", "Raw-Salmon");
+  } else if (randNum < tropicalProb) {
+    caughtFish("tropical", "Tropical-Fish");
+  } else {
+    caughtFish("puffer", "Pufferfish");
+  }
+}
+
+// Add to counter and update HTML
+function caughtFish(fishName, imgName) {
+  numFish[fishName]++;
+  spans[fishName].innerHTML = numFish[fishName];
+  resultImg.src = `img/${imgName}.png`;
 }
